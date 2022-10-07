70c94fd841e3c72014c1b703331eaaab12e8e3c7
Estudiante <Estudiante@lab0717>
2022-10-07 11:26:47 -0500

    var act

---
+++ src/main/java/com/mycompany/p1u1agudo_clavijo_fajardo/Auto.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.p1u1agudo_clavijo_fajardo;
+
+/**
+ *
+ * @author esteb
+ */
+public class Auto {
+    
+    public String marcaAuto;
+    public int añoAuto;
+    public int puertasAuto;
+    public String tipoAuto;
+}
---
+++ src/main/java/com/mycompany/p1u1agudo_clavijo_fajardo/Perro.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.p1u1agudo_clavijo_fajardo;
+
+/**
+ *
+ * @author esteb
+ */
+public class Perro {
+    
+    public String razaPerro;
+    public String nombrePerro;
+    public int edadPerro;
+    public String sexoPerro;
+    
+}
