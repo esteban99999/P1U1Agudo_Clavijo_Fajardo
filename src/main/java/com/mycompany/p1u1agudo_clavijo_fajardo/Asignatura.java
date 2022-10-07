9892f5733d5ff6c6076be2e21840162857608b14
Estudiante <Estudiante@lab0717>
2022-10-07 11:26:37 -0500

    var act

---
+++ src/main/java/com/mycompany/p1u1agudo_clavijo_fajardo/Asignatura.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.p1u1agudo_clavijo_fajardo;
+
+/**
+ *
+ * @author esteb
+ */
+public class Asignatura {
+    
+    public String nombreAsignatura;
+    public int horasAsignatura;
+    public String profesorAsignatura;
+    public String jornadaAsignatura;
+}
